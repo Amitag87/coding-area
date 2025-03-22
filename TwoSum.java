83602697edf9848ee2670c02fd9ed0ce659a9b5e
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2025-03-22 15:15:33 +0530

    update

---
+++ TwoSum.java
@@ -0,0 +1,41 @@
+//TWO SUM LC-01
+
+
+//Approach 1 TC->O(n^2)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int ans[]=new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1 ;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
+//Approach 2  TC->O(n)
+
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int remain;
+        for(int i=0;i<nums.length;i++){
+            remain=target-nums[i];
+            if(map.containsKey(remain)){
+                return new int[]{
+                        map.get(remain) , i
+                } ;
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
