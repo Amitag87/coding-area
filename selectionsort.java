fe3c4fc8c2ea3657cb0486f7923adf24ba932044
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2025-06-20 22:16:27 +0530

    update

--- selectionsort.java
+++ selectionsort.java
@@ -13,18 +13,20 @@ public class Main
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    int n = sc.nextInt();
-		int[] arr =0 new int[n];
+		int[] arr = new int[n];
 		for(int i=0;i<n;i++){
 		    arr[i]=sc.nextInt();
 		}
 		for(int i=0;i<n-1;i++){
-		    for(int j=0;j<n-i-1;j++){
-		        if(arr[j]>arr[j+1]){
-		            int temp=arr[j];
-		            arr[j]=arr[j+1];
-		            arr[j+1]=temp;
+		    int min=i;
+		    for(int j=i+1;j<n;j++){
+		        if(arr[j]<arr[min]){
+		            min=j;
 		        }
 		    }
+		    int temp=arr[i];
+		    arr[i]=arr[min];
+		    arr[min]=temp;
 		}
 		for(int i=0;i<n;i++){
 		    System.out.print(arr[i]+" ");
